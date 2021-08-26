e31f02fadec38086157670f1d74ab47b3b03c937
Natsuki <natalie@monkvision.ai>
2021-08-26 20:05:52 +0200

    Add a beautiful Loader component

--- App.jsx
+++ App.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
 
 import { Provider as PaperProvider } from 'react-native-paper';
 import { Provider as StoreProvider } from 'react-redux';
 
 import { store } from 'Controllers';
 
-import Logo from 'Components/Logo';
+import Loader from 'Components/Loader';
 
 const styles = StyleSheet.create({
   container: {
@@ -16,14 +16,29 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  loader: {
+    height: 100,
+    width: 100,
+  },
 });
 
 export default function App() {
+  const [isLoading, setLoading] = useState(false);
+
+  function handlePress() {
+    setLoading((prevState) => !prevState);
+  }
+
   return (
     <StoreProvider store={store}>
       <PaperProvider theme={store.getState().theme}>
         <View style={styles.container}>
-          <Logo height={100} width={100} />
+          <Pressable onPress={handlePress}>
+            <Loader
+              isLoading={isLoading}
+              style={styles.loader}
+            />
+          </Pressable>
           <Text style={{ marginTop: 16 }}>
             Welcome to Monk Software Development Kit!
           </Text>
