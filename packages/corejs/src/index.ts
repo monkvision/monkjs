b171d4cea2b9eaf9abe760fd2c91f84d30b5012a
franklinkemta <franklin@monkvision.ai>
2021-10-29 14:36:39 +0100

    [MWIP] Add types and configs

---
+++ packages/corejs/src/index.ts
@@ -0,0 +1,51 @@
+import { fetchBaseQuery } from "@reduxjs/toolkit/query";
+import getInspectionApi from "./services/inspection";
+
+export { default as values } from "./values";
+export { default as Sight } from "./classes/Sight";
+
+export function getBaseQuery({
+  baseUrl,
+  customHeaders = [],
+}: {
+  baseUrl: string;
+  customHeaders: any;
+}) {
+  return fetchBaseQuery({
+    baseUrl,
+    prepareHeaders: (headers: any, { getState }: any) => {
+      const token = getState().auth.accessToken;
+
+      headers.set("Access-Control-Allow-Origin", "*");
+      customHeaders.forEach(([key, value]: [string, string]) => {
+        headers.set(key, value);
+      });
+
+      if (token) {
+        headers.set("authorization", `Bearer ${token}`);
+      }
+
+      return headers;
+    },
+  });
+}
+
+class MonkCore {
+  _baseQuery: string;
+  _inspection: any;
+
+  constructor(baseQuery: string) {
+    this._baseQuery = baseQuery;
+    this._inspection = getInspectionApi(baseQuery);
+  }
+
+  get baseQuery() {
+    return this._baseQuery;
+  }
+
+  get inspection() {
+    return this._inspection;
+  }
+}
+
+export default MonkCore;
