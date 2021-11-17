5203c05bb6f615e36e92fcd948cc8e51f02a0d73
Ilyass Ben Hakim <ilyassbenhakim2@gmail.com>
2021-11-17 16:55:50 +0100

    Added signout callback into useAuth hook

--- src/hooks/useAuth/index.js
+++ src/hooks/useAuth/index.js
@@ -1,19 +1,36 @@
-import { useMemo } from 'react';
+import auth0Discovery from 'config/discoveries';
+import { revokeAsync } from 'expo-auth-session';
+import Constants from 'expo-constants';
+import { useCallback, useMemo, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { set } from 'store';
+import { authSlice } from 'store/slices/auth';
 
-const authIntialState = {
-  accessToken: null,
-  isAuthenticated: false,
-  isLoading: false,
-  isSignedOut: true,
-};
 export default function useAuth() {
   const auth = useSelector((state) => state.auth);
   const dispatch = useDispatch();
-
   const isAuthenticated = useMemo(() => Boolean(auth.accessToken), [auth]);
-  const signout = () => dispatch(set(authIntialState));
 
-  return { ...auth, signout, isAuthenticated };
+  // signout
+  const [isLoggingOut, setLoggingOut] = useState(false);
+
+  const signout = useCallback(async () => {
+    setLoggingOut(true);
+    const clientId = Constants.manifest.extra.AUTH_CLIENT_ID;
+    try {
+      await revokeAsync(
+        {
+          client_id: clientId,
+          token: auth.accessToken,
+          tokenType: auth.tokenType,
+        },
+        auth0Discovery,
+      );
+
+      dispatch(authSlice.actions.reset({ isSignedOut: true }));
+    } catch (e) {
+      setLoggingOut(false);
+    }
+  }, [auth.accessToken, auth.tokenType, dispatch]);
+
+  return { ...auth, signout, isLoggingOut, isAuthenticated };
 }
