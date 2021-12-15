348801e66238049db9422beea9c85a4431ee3257
Ilyass Ben Hakim <ilyassbenhakim2@gmail.com>
2021-12-15 18:13:37 +0100

    Handling Android support for Image events

--- src/components/Img/index.jsx
+++ src/components/Img/index.jsx
@@ -17,7 +17,9 @@ const styles = StyleSheet.create({
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
+    zIndex: 2,
   },
+  skeleton: { position: 'absolute', zIndex: 1 },
 });
 
 export default function Img({ style, skeletonStyle, ...props }) {
@@ -41,7 +43,7 @@ export default function Img({ style, skeletonStyle, ...props }) {
     handleAnimateOpacity(1, imageOpacity).start();
   }, [imageOpacity, placeholderOpacity, handleAnimateOpacity]);
 
-  const handleShowErrorCard = useCallback(() => {
+  const onError = useCallback(() => {
     // hide placeholder
     handleAnimateOpacity(0, placeholderOpacity).start();
     // show error card
@@ -51,12 +53,23 @@ export default function Img({ style, skeletonStyle, ...props }) {
     ]).start();
   }, [errorCardOpacity, handleAnimateOpacity, placeholderOpacity]);
 
+  // onError doesn't trigger on IOS that's why we put it into onLoadEnd
+  const events = Platform.select({
+    default: { onError },
+    ios: { onLoadEnd: onError },
+  });
+
   return (
     <View style={[style, { position: 'relative' }]}>
       {/* the image is wrapped into an animated
        view to make the image style manipulation natural */}
       <Animated.View style={{ opacity: imageOpacity, position: 'absolute', zIndex: 10 }}>
-        <Image style={style} onLoad={onLoad} onLoadEnd={handleShowErrorCard} {...props} />
+        <Image
+          style={style}
+          onLoad={onLoad}
+          {...events}
+          {...props}
+        />
       </Animated.View>
 
       {/* show this card when no image loaded  */}
@@ -75,7 +88,7 @@ export default function Img({ style, skeletonStyle, ...props }) {
         <Text style={styles.text}>Broken image</Text>
       </Animated.View>
 
-      <Animated.View style={{ opacity: placeholderOpacity, position: 'absolute' }}>
+      <Animated.View style={[styles.skeleton, { opacity: placeholderOpacity }]}>
         <Placeholder style={skeletonStyle} />
       </Animated.View>
     </View>
