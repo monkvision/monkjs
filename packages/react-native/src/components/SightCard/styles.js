2dcb54681603ccad9ec37d69296e9fc654701c5a
Ilyass Ben Hakim <ilyassbenhakim2@gmail.com>
2021-12-28 13:39:15 +0100

    Fixed style warning from SightCard

--- packages/react-native/src/components/SightCard/styles.js
+++ packages/react-native/src/components/SightCard/styles.js
@@ -36,9 +36,7 @@ const styles = StyleSheet.create({
     right: 0,
     zIndex: 10,
     color: 'white',
-
-    // NOTE(Ilyass): The borderBottom, logs a warning (to be fixed)
-    borderBottom: 0,
+    borderBottomWidth: 0,
     position: 'absolute',
     backgroundColor: '#ff9800',
   },
