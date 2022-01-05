9b41eff4ca66162be0049b470bb58e22ce2d9104
Ilyass Ben Hakim <ilyassbenhakim2@gmail.com>
2022-01-05 17:04:54 +0100

    Handling large screen case

--- packages/react-native-views/src/components/CreateDamageForm/DamageRow/index.js
+++ packages/react-native-views/src/components/CreateDamageForm/DamageRow/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { View, Text, StyleSheet } from 'react-native';
 import { IconButton, DataTable } from 'react-native-paper';
 import { startCase } from 'lodash';
+import { useMediaQuery } from 'react-responsive';
 
 const styles = StyleSheet.create({
   cell: {
@@ -18,21 +19,25 @@ const styles = StyleSheet.create({
   title: { maxWidth: 100 },
 });
 
-const MAX_POSSIBLE_LENGTH = 25;
-const handleValueWidth = (value) => {
-  if (value?.length > MAX_POSSIBLE_LENGTH) {
-    return `${startCase(value?.substring(0, 20))}...`;
-  }
-  return startCase(value);
-};
+// width < 350 ? MAX_POSSIBLE_LENGTH : Infinity
 
 export default function DamageRow({ value, title, ...rest }) {
+  const isMobile = useMediaQuery({ query: '(max-device-width: 350px)' });
+
+  const handleValueWidth = useMemo(() => {
+    const MAX_POSSIBLE_LENGTH = isMobile ? 25 : Infinity;
+    if (value?.length > MAX_POSSIBLE_LENGTH) {
+      return `${startCase(value?.substring(0, 20))}...`;
+    }
+    return startCase(value);
+  }, [value, isMobile]);
+
   return (
     <DataTable.Row {...rest}>
       <DataTable.Cell style={styles.title}>{title}</DataTable.Cell>
       <DataTable.Cell style={styles.alignLeft}>
         <View style={styles.cell}>
-          <Text>{handleValueWidth(value) || 'Not given'}</Text>
+          <Text>{handleValueWidth || 'Not given'}</Text>
           <IconButton icon="pencil" disabled />
         </View>
       </DataTable.Cell>
