ffde7d60be78f87aa11751bfe2a839d47b6d51ae
Natsuki <natalie@monkvision.ai>
2022-01-28 15:47:09 +0100

    Remove CopyToClipboard

--- website/src/pages/Sights/index.js
+++ website/src/pages/Sights/index.js
@@ -9,11 +9,7 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Chip from '@mui/material/Chip';
-import Snackbar from '@mui/material/Snackbar';
 import Typography from '@mui/material/Typography';
-
-import BrowserOnly from '@docusaurus/BrowserOnly';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
 import ReactJson from 'react-json-view';
 
 import sightsData from '@monkvision/sights/dist';
@@ -21,20 +17,6 @@ import sightsData from '@monkvision/sights/dist';
 // eslint-disable-next-line react/prop-types
 function SightCard({ id, label, category, vehicleType, overlay }) {
   const base64 = btoa(unescape(encodeURIComponent(overlay)));
-
-  const [open, setOpen] = React.useState(false);
-
-  const handleCopy = () => {
-    setOpen(true);
-  };
-
-  const handleClose = (event, reason) => {
-    if (reason === 'clickaway') {
-      return;
-    }
-
-    setOpen(false);
-  };
 
   return (
     <Card sx={{
@@ -61,9 +43,7 @@ function SightCard({ id, label, category, vehicleType, overlay }) {
         >
           {/* eslint-disable-next-line react/prop-types */}
           {label.charAt(0).toUpperCase() + label.slice(1)}
-          <CopyToClipboard text={id} onCopy={handleCopy}>
-            <Chip label={id} />
-          </CopyToClipboard>
+          <Chip label={id} />
         </Typography>
         <ReactJson
           style={{
@@ -78,13 +58,6 @@ function SightCard({ id, label, category, vehicleType, overlay }) {
           collapseStringsAfterLength={20}
         />
       </CardContent>
-      <Snackbar
-        open={open}
-        autoHideDuration={6000}
-        onClose={handleClose}
-        message={`${id} copied!`}
-        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-      />
     </Card>
   );
 }
@@ -123,8 +96,4 @@ function Sights() {
   );
 }
 
-export default (props) => (
-  <BrowserOnly>
-    {() => <Sights {...props} />}
-  </BrowserOnly>
-);
+export default Sights;
