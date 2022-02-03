f3d8918efb6461c00b7ddd6baa129db131360660
Natsuki <natalie@monkvision.ai>
2022-02-03 19:54:31 +0100

    Add test screen view

--- src/screens/Test/index.js
+++ src/screens/Test/index.js
@@ -1,10 +1,46 @@
+// import React, { useCallback, useState } from 'react';
+// import { Capture, Controls } from '@monkvision/camera';
+// import { SafeAreaView, StatusBar } from 'react-native';
+//
+// export default function Test() {
+//   const [loading, setLoading] = useState();
+//
+//   const handleCapture = useCallback(async (api) => {
+//     const { takePictureAsync, startUploadAsync, goNextSight } = api;
+//
+//     setLoading(true);
+//     const { picture } = await takePictureAsync();
+//
+//     setTimeout(() => {
+//       setLoading(false);
+//       goNextSight();
+//       startUploadAsync(picture);
+//     }, 200);
+//   }, []);
+//
+//   const controls = [{
+//     disabled: loading,
+//     onPress: handleCapture,
+//     ...Controls.CaptureButtonProps,
+//   }];
+//
+//   return (
+//     <SafeAreaView>
+//       <StatusBar hidden />
+//       <Capture
+//         inspectionId="b5f85f7d-6722-8690-b592-fd02600496f2"
+//         controls={controls}
+//         loading={loading}
+//       />
+//     </SafeAreaView>
+//   );
+// }
+
 import React from 'react';
-import { StatusBar, SafeAreaView } from 'react-native';
+import { SafeAreaView } from 'react-native';
 
 export default function Test() {
   return (
-    <SafeAreaView>
-      <StatusBar hidden />
-    </SafeAreaView>
+    <SafeAreaView />
   );
 }
