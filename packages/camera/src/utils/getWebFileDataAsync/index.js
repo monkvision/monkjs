8ce5bcac47f368ffe4e5355fd2597052defdfc96
Natsuki <natalie@monkvision.ai>
2022-02-18 18:29:19 +0100

    Fix camera upload

--- packages/camera/src/utils/getWebFileDataAsync/index.js
+++ packages/camera/src/utils/getWebFileDataAsync/index.js
@@ -21,7 +21,7 @@ export default async function getWebFileData(blob, sights, inspectionId, setting
 
   const json = JSON.stringify({
     acquisition: { strategy: 'upload_multipart_form_keys', file_key: multiPartKeys.image },
-    compliances: { iqc_compliance: {} },
+    // compliances: { iqc_compliance: {} },
     tasks: ['damage_detection'],
     additional_data: {
       ...sights.state.current.metadata,
