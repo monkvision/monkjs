cea5ed77f9efee20cd4b3c2dbb1b47f5806ed6ad
Ilyass Ben Hakim <ilyassbenhakim2@gmail.com>
2022-03-10 15:46:53 +0100

    Disable ignoreLogs due to blocking thrown errors

--- src/config/ignoreLogs.js
+++ src/config/ignoreLogs.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 import { Platform, LogBox } from 'react-native';
 
+// sometimes it blocks throwm errors
 export default function ignoreLogs(supressedWarnings, severity = 'error') {
   if (Platform.OS !== 'web') { LogBox.ignoreLogs(supressedWarnings); return; }
 
