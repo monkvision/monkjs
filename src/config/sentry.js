b4a1e44b5be749ca87f041497e0a6bd07489a877
Ilyass Ben Hakim <ilyassbenhakim2@gmail.com>
2022-04-28 11:30:40 +0000

    Tried more tests for native

--- src/config/sentry.js
+++ src/config/sentry.js
@@ -13,6 +13,7 @@ if (!isEmpty(dsn)) {
     // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
     // We recommend adjusting this value in production.
     tracesSampleRate: 1.0,
+    environment: Constants.manifest.extra.ENV,
   });
 }
 
