219be391a96a02b98869eed54255100293e9c0fa
Ilyass Ben Hakim <ilyassbenhakim2@gmail.com>
2022-05-05 21:46:24 +0000

    Added capture a blocking step before going to upload center, awaiting the compliances to finish

--- packages/camera/src/components/Capture/capture.js
+++ packages/camera/src/components/Capture/capture.js
@@ -194,6 +194,12 @@ const Capture = forwardRef(({
     () => !Object.values(uploads.state).some((upload) => !upload.picture),
     [uploads.state],
   );
+  // this will block the user from going to upload center, until we fix the queue
+  
+  const complianceHasFulfilledAll = useMemo(
+    () => Object.values(compliance.state).every(({ status }) => status !== 'idle' && status !== 'pending'),
+    [compliance.state],
+  );
   const overlaySize = useMemo(
     () => utils.styles.getSize('4:3', windowDimensions, 'number'),
     [windowDimensions],
@@ -284,7 +290,7 @@ const Capture = forwardRef(({
     </>
   ), [isReady, loading, overlay, overlaySize, primaryColor]);
 
-  if (enableComplianceCheck && tourHasFinished) {
+  if (enableComplianceCheck && tourHasFinished && complianceHasFulfilledAll) {
     return (
       <UploadCenter
         {...states}
--- packages/camera/src/components/Controls/index.js
+++ packages/camera/src/components/Controls/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/no-array-index-key */
-import React, { createElement, useCallback } from 'react';
+import React, { createElement, useCallback, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { Platform, StyleSheet, Text, TouchableOpacity, useWindowDimensions, View } from 'react-native';
 import useHandlers from './hooks';
@@ -41,6 +41,11 @@ export default function Controls({
     enableComplianceCheck,
   });
 
+  const hasNoIdle = useMemo(
+    () => Object.values(state.uploads.state).every(({ status }) => status !== 'idle'),
+    [state.uploads],
+  );
+
   const handlePress = useCallback(({ onPress }) => (e) => {
     if (typeof onPress === 'function') { onPress(state, api, e); } else { handlers.capture(state, api, e); }
   }, [api, handlers, state]);
@@ -61,7 +66,7 @@ export default function Controls({
       }, i) => (
         createElement(component, {
           key: `camera-control-${i}`,
-          disabled: loading,
+          disabled: loading || hasNoIdle,
           onPress: handlePress({ onPress, ...rest }),
           style: StyleSheet.flatten([styles.button]),
           ...rest,
@@ -92,6 +97,7 @@ Controls.propTypes = {
   onFinishUploadPicture: PropTypes.func,
   onStartUploadPicture: PropTypes.func,
   state: PropTypes.shape({
+    compliance: PropTypes.objectOf(PropTypes.any),
     settings: PropTypes.objectOf(PropTypes.any),
     sights: PropTypes.objectOf(PropTypes.any),
     uploads: PropTypes.objectOf(PropTypes.any),
