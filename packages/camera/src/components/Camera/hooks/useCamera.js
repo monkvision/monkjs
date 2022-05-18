342e7c3c61096219ff18b8bf79d6cc7457114a01
Ilyass Ben Hakim <ilyassbenhakim2@gmail.com>
2022-05-18 12:39:21 +0100

    Removed the base canvas, and added a condition to not compress image under 3mb

--- packages/camera/src/components/Camera/hooks/useCamera.js
+++ packages/camera/src/components/Camera/hooks/useCamera.js
@@ -4,19 +4,18 @@ import { utils } from '@monkvision/toolkit';
 import useUserMedia from './useUserMedia';
 
 // get url from canvas blob, because `canvas.toDataUrl` can't be revoked programmatically
-const toBlob = (canvasElement, type, quality = 1) => new Promise((resolve) => {
-  canvasElement.toBlob((blob) => resolve(URL.createObjectURL(blob)), type, quality);
+const toBlob = (canvasElement, type) => new Promise((resolve) => {
+  canvasElement.toBlob((blob) => resolve(URL.createObjectURL(blob)), type, 0.02);
 });
 
 /**
  * Note(Ilyass): As a solution I increased the `baseCanvas` and the video constraints resolution
- * to `2561x1441` and created another canvas called `croppedCanvas` which will be taking picture
+ * to `2561x1441` and created another canvas called `canvas` which will be taking picture
  * based on `baseCanvas` (not on the `video.current`) while holding a resolution of
  * `2560x1440`, that why we added the `diff`.
  */
 const diff = 1;
-const baseCanvas = document.createElement('canvas');
-const croppedCanvas = document.createElement('canvas');
+const canvas = document.createElement('canvas');
 const imageType = utils.supportsWebP ? 'image/webp' : 'image/png';
 
 /**
@@ -43,20 +42,16 @@ export default function useCamera({ width, height }, options) {
 
   // we can set the canvas dimensions one time rather than on every time we press capture
   useEffect(() => {
-    baseCanvas.width = width + diff;
-    baseCanvas.height = height + diff;
-
-    croppedCanvas.width = width;
-    croppedCanvas.height = height;
+    canvas.width = width;
+    canvas.height = height;
   }, [width, height]);
 
   const takePicture = useCallback(async () => {
     if (!videoRef.current || !stream) { throw new Error('Camera is not ready!'); }
 
-    baseCanvas.getContext('2d').drawImage(videoRef.current, 0, 0, width, height);
-    croppedCanvas.getContext('2d').drawImage(baseCanvas, 0, 0, width, height);
+    canvas.getContext('2d').drawImage(videoRef.current, 0, 0, width, height);
 
-    const uri = await toBlob(croppedCanvas, imageType);
+    const uri = await toBlob(canvas, imageType);
 
     return { uri };
   }, [width, height, stream]);
