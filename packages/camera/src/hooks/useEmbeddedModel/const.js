06b53bd6d5161281f854b061ae90bf7865655847
alexandre-em <alexandre.em@monkvision.ai>
2022-07-21 13:04:33 +0200

    Update iqa compliance result to fit results given by api

--- packages/camera/src/hooks/useEmbeddedModel/const.js
+++ packages/camera/src/hooks/useEmbeddedModel/const.js
@@ -2,9 +2,17 @@ const Models = {
   imageQualityCheck: {
     name: 'imageQualityCheck',
     uri: 'https://embedded-models.dev.monk.ai/IQA/Multitask/2022-04-12-mobilevit_xs/v0/checkpoints/epoch_7-val_loss%3D0.023.ckpt.onnx.savedmodel.tflite',
+    minConfidence: {
+      blurriness: 0.4,
+      overexposure: 0.8,
+      underexposure: 0.4,
+    },
   },
   partDetectorModel: {
     name: 'partDetector',
+    minConfidence: {
+      partDetector: 0.4,
+    },
   },
 };
 
