e9d945f1f0cc24757de90a5ae43a9c30a8177a48
souyahia-monk <samy.ouyahia@monkvision.ai>
2022-08-25 11:00:22 +0200

    Added missing sights and cleaned up all sights

--- packages/sights/check_sights.py
+++ packages/sights/check_sights.py
@@ -17,9 +17,18 @@ def main():
     folder = base / 'assets/overlays'
     files = set(file.stem for file in folder.iterdir())
     ids_in_json = set(json.loads((base / 'index.json').read_bytes()).keys())
+    not_minified = []
+    for file in folder.iterdir():
+        num_lines = sum(1 for line in open(file))
+        if num_lines > 1:
+            not_minified.append(file.stem)
 
     if any(
-        [check("Missing SVG overlays", ids_in_json - files), check("Missing info in index.json", files - ids_in_json),]
+        [
+            check("Missing SVG overlays", ids_in_json - files),
+            check("Missing info in index.json", files - ids_in_json),
+            check("Not minified", set(not_minified)),
+        ]
     ):
         print(file=sys.stderr)
         print(f"SVG overlays: {len(files)}", file=sys.stderr)
