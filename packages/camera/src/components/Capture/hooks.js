de29e60f0104edffd70bbf916e7ae5e4e7d3ddfa
souyahia-monk <samy.ouyahia@monkvision.ai>
2022-11-30 18:10:40 +0100

    feat: Disabled car coverage

--- packages/camera/src/components/Capture/hooks.js
+++ packages/camera/src/components/Capture/hooks.js
@@ -280,7 +280,7 @@ export function useStartUploadAsync({
         },
         compliances: {
           image_quality_assessment: {},
-          coverage_360: { sight_id: id },
+          // coverage_360: { sight_id: id },
           // coverage_360: COVERAGE_360_WHITELIST.includes(id) ? {
           //   sight_id: id,
           // } : undefined,
