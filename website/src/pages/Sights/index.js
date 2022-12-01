51f0fc962da6ef56cf5c5947ddc31722fd997eb7
Rohit Sharma <rohit.sharma@globant.com>
2022-12-01 13:51:44 +0530

    add functionality for filter and search

--- website/src/pages/Sights/index.js
+++ website/src/pages/Sights/index.js
@@ -1,64 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import BrowserOnly from '@docusaurus/BrowserOnly';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
 import Layout from '@theme/Layout';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import Container from '@mui/material/Container';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Chip from '@mui/material/Chip';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
+import { Container, CssBaseline } from '@mui/material';
 
 import sightsData from '@monkvision/sights/dist';
-
-console.log(sightsData);
-
-// eslint-disable-next-line react/prop-types
-function SightCard({ id, label, category, vehicleType, overlay }) {
-  const base64 = btoa(unescape(encodeURIComponent(overlay)));
-
-  return (
-    <Card sx={{
-      width: '100%',
-      maxWidth: '320px',
-      margin: 2,
-    }}
-    >
-      <CardMedia
-        component="img"
-        alt={label}
-        height="240"
-        image={`data:image/svg+xml;base64,${base64}`}
-      />
-      <CardContent sx={{ textAlign: 'left' }}>
-        <Typography
-          gutterBottom
-          variant="h6"
-          component="div"
-          sx={{
-            display: 'flex',
-            justifyContent: 'space-between',
-          }}
-        >
-          {/* eslint-disable-next-line react/prop-types */}
-          {label.en ? label.en.charAt(0).toUpperCase() + label.en.slice(1) : 'No label'}
-          <Chip label={id} />
-        </Typography>
-      </CardContent>
-      <Box component="pre" sx={{ backgroundColor: 'black', borderRadius: 0 }}>
-        {JSON.stringify({ id, label, category, vehicleType }, null, 2)}
-      </Box>
-    </Card>
-  );
-}
+// import sightsData from '../../../../packages/sights';
+import SightCard from './SightCard';
+import FiltersForm from './FiltersForm';
+import styles from '../styles.module.css';
 
 function Sights() {
   const context = useDocusaurusContext();
   const { siteConfig: { customFields = {}, tagline } = {} } = context;
+  const [sights, setSights] = useState(Object.values(sightsData));
+  const [search, setSearch] = useState('');
+  const [category, setCategory] = useState('');
 
   const darkTheme = createTheme({
     palette: {
@@ -66,26 +25,52 @@ function Sights() {
     },
   });
 
+  useEffect(() => {
+    const filteredSights = Object.values(sightsData)
+      .filter((sight) => (category === '' || category === sight.category))
+      .filter((sight) => {
+        const value = JSON.stringify(sight).toLocaleLowerCase();
+        return typeof value === 'string' && value.includes(search.toLocaleLowerCase());
+      });
+    setSights(filteredSights);
+  }, [search, category]);
+
   return (
     <ThemeProvider theme={darkTheme}>
       <Layout title={tagline} description={customFields.description}>
+        <Container
+          className={styles.filtersBand}
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            flexWrap: 'wrap',
+            justifyContent: 'flex-end',
+            paddingBottom: 2,
+          }}
+          maxWidth={false}
+          disableGutters
+        >
+          <FiltersForm onCategoryChange={setCategory} onSearchChange={setSearch} />
+        </Container>
         <Container
           sx={{
             display: 'flex',
             flexDirection: 'row',
             flexWrap: 'wrap',
             justifyContent: 'center',
+            paddingTop: '70px',
             paddingBottom: 2,
+            backgroundColor: '#121212',
           }}
           maxWidth={false}
           disableGutters
         >
           <CssBaseline />
-          {Object.values(sightsData)
+          {Object.values(sights)
             .filter((sight) => !!sight.overlay)
             .map((sight) => (
-              <BrowserOnly>
-                {() => <SightCard key={sight.id} {...sight} />}
+              <BrowserOnly key={sight.id}>
+                {() => <SightCard {...sight} />}
               </BrowserOnly>
             ))}
         </Container>
