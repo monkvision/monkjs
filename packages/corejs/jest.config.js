c32ba36debdf962a7097700f211eddabb809feaa
souyahia-monk <samy.ouyahia@monkvision.ai>
2022-12-09 16:20:19 +0100

    fix: Changed jest test env

--- packages/corejs/jest.config.js
+++ packages/corejs/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   testMatch: ['**/test/**/*.test.ts'],
   coverageDirectory: './coverage',
   coverageReporters: ['lcov'],
