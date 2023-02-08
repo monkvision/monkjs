f8ec4c54bd3e62b2161fdbdb676e680898145025
souyahia-monk <samy.ouyahia@monkvision.ai>
2023-02-08 15:08:15 +0100

    Removed language detection and forced the Webapp to be in English by default

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,13 +1,12 @@
 import i18n from 'i18next';
 import { i18nCamera } from '@monkvision/camera';
 import { initReactI18next } from 'react-i18next';
-import * as Localization from 'expo-localization';
 import resources from './resources';
 
 i18n.use(initReactI18next).init({
   compatibilityJSON: 'v3',
   fallbackLng: 'en',
-  lng: Localization.locale,
+  lng: 'en',
   interpolation: {
     escapeValue: false,
   },
