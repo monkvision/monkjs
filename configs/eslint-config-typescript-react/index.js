f8ece9ffe4fdc2e41edbb6eb248137e657113b2b
Rohit Sharma <rohit.sharma@globant.com>
2023-03-23 01:06:44 +0530

    change the code as per given feedback on PR. Remove some unwanted dependencies from esling-config-base as well.

--- configs/eslint-config-typescript-react/index.js
+++ configs/eslint-config-typescript-react/index.js
@@ -5,37 +5,15 @@ const ERROR = 2;
 module.exports = {
   root: true,
   extends: [
+    'react-app',
+    'react-app/jest',
     '@monkvision/eslint-config-typescript',
   ],
-  plugins: ['jest', '@typescript-eslint', 'import', 'prettier'],
   parser: '@typescript-eslint/parser',
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
-    },
-  },
   rules: {
-    'prettier/prettier': ERROR,
+    'indent': ['error', 2],
     'no-console': ERROR,
     'react/function-component-definition': [ERROR, { namedComponent: 'arrow-function' }],
   },
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      rules: {
-        '@typescript-eslint/no-shadow': [ERROR],
-        'no-shadow': OFF,
-        'no-undef': OFF,
-      },
-    },
-    {
-      files: ['test/**/*.{ts,tsx}'],
-      rules: {
-        'import/first': OFF,
-      },
-    },
-  ],
   ignorePatterns: ['**/*.js', 'node_modules', 'dist'],
 };
