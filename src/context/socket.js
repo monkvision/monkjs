6ff123d805594cf62e9dc102320080c3812b51cb
Rohit Sharma <rohit.sharma@globant.com>
2023-04-05 21:23:26 +0530

    Refactor code in React context API for web-socket set up

--- src/context/socket.js
+++ src/context/socket.js
@@ -1,55 +1,67 @@
-import React, { createContext, useContext, useMemo } from 'react';
+import React, { createContext, useContext, useCallback, useMemo, useState, useEffect } from 'react';
 import { io } from 'socket.io-client';
 
 /**
- * Creates a socket.io client that connects to the server. Replace url with your server URL
+ * Creates a socket.io client that connects to the server
  * Make sure to enable Cross-Origin Resource Sharing (CORS) on the server
  * The options parameter is optional and can be used to configure the connection
  */
-const socket = io('http://localhost:3000', {});
-socket.on('connect', () => {
-  console.log('Socket connected', socket.id);
-});
-socket.on('disconnect', (reason) => {
-  console.log('Socket disconnected due to', reason);
-});
-socket.on('connect_error', () => {
-  setTimeout(() => {
-    socket.connect();
-  }, 1000);
-});
+const socket = io('http://localhost:5000', {}); // Replace url with your server URL
 
-// React context for socket
+// React context for web socket data
 const SocketContext = createContext({
   onSocketEvent: () => {},
   emitSocketEvent: () => {},
 });
 
 function SocketProvider({ children }) {
-  const onSocketEvent = (event, callback) => {
+  const [socketID, setSocketID] = useState('');
+
+  const onSocketEvent = useCallback((event, callback) => {
     if (socket.connected) {
       socket.on(event, () => {
         socket.off(event);
         callback();
       });
     }
-  };
+  }, []);
 
-  const emitSocketEvent = (event, args = {}, callback = () => {}) => {
+  const emitSocketEvent = useCallback((event, args = {}, callback = () => {}) => {
     if (socket.connected) {
       socket.emit(event, args, () => {
-        console.log('Successfully emitted an event named -', event);
+        console.log('[Socket] - emit event', event);
         callback();
       });
     }
-  };
+  }, []);
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      console.log('[Socket] - connected', socket);
+      console.log('[Socket] - socket id', socket.id);
+      setSocketID(socket.id);
+      socket.on('post_inspection', (data) => {
+        console.log('[Socket] - listen post_inspection event from server', data);
+      });
+    });
+    socket.on('disconnect', (reason) => {
+      console.log('[Socket] - disconnected due to', reason);
+    });
+    socket.on('connect_error', () => {
+      console.log('[Socket] - has a connection error');
+      setTimeout(() => {
+        socket.connect();
+      }, 1000);
+    });
+  }, []);
 
   const value = useMemo(() => (
     {
+      socketID,
       onSocketEvent,
       emitSocketEvent,
     }
-  ), []);
+  ), [socketID]);
 
   return (
     <SocketContext.Provider value={value}>
