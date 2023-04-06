175729da2ce1c8140969505c0e97e90d61f7a2f3
Rohit Sharma <rohit.sharma@globant.com>
2023-04-06 17:43:47 +0530

    fix an issue as socket_id field isn't in schema

--- src/screens/InspectionCreate/useCreateInspection/index.js
+++ src/screens/InspectionCreate/useCreateInspection/index.js
@@ -4,7 +4,7 @@ import useAuth from 'hooks/useAuth';
 import { useCallback, useState } from 'react';
 import { useDispatch } from 'react-redux';
 
-export default function useCreateInspection(vehicle) {
+export default function useCreateInspection({ socketID, ...vehicle }) {
   const dispatch = useDispatch();
   const { isAuthenticated } = useAuth();
   const [inspectionId, setInspectionId] = useState();
