7cf5d6c14c6356f07b73240b2f1a206c85f6ac55
rohitsharma120582 <86062128+rohitsharma120582@users.noreply.github.com>
2023-04-18 10:51:31 +0530

    V&M Main Component  (#475)
    * add new packages for vector icons
    
    * create a common component for button only with icon
    
    * create a component for Tabs
    
    * create main component for required UI
    
    * use main component only in landing page

--- packages/inspection-report/src/index.js
+++ packages/inspection-report/src/index.js
@@ -1,2 +1,2 @@
 // eslint-disable-next-line import/prefer-default-export
-export { default as TestView } from './components/TestView';
+export { default as DamageReport } from './components/DamageReport';
