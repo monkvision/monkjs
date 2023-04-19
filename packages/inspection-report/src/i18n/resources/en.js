29685fe98d47b92df791b75f49378e171fa3ddcb
rohitsharma120582 <86062128+rohitsharma120582@users.noreply.github.com>
2023-04-19 14:53:36 +0530

    Feature/mn 252/i18next (#478)
    * add new package for localization
    
    * initialize locatization using i18n
    
    * provide localization to react components
    
    * add localization instance for inspecrtion-report package to change the language from demo app
    
    * Added french translations
    
    * pass down props to DamageReport component received as props from main component
    
    ---------
    
    Co-authored-by: Samy Ouyahia <103439265+souyahia-monk@users.noreply.github.com>

---
+++ packages/inspection-report/src/i18n/resources/en.js
@@ -0,0 +1,17 @@
+const en = {
+  translation: {
+    damageReport: {
+      title: 'Damage Report',
+      tabs: {
+        overview_tab: {
+          label: 'Overview',
+        },
+        photos_tab: {
+          label: 'Photos',
+        },
+      },
+    },
+  },
+};
+
+export default en;
