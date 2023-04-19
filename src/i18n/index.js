975797c859d0abe840bc440ddc493859f54208f1
Rohit Sharma <rohit.sharma@globant.com>
2023-04-19 12:20:56 +0530

    add localization instance for inspecrtion-report package to change the language from demo app

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,5 +1,6 @@
 import i18n from 'i18next';
 import { i18nCamera } from '@monkvision/camera';
+import { i18nDamageReport } from '@monkvision/inspection-report';
 import { initReactI18next } from 'react-i18next';
 import * as Localization from 'expo-localization';
 import resources from './resources';
@@ -19,6 +20,7 @@ i18n.use(initReactI18next).init({
 i18n.on(
   'languageChanged',
   (lng) => i18nCamera.changeLanguage(lng).catch((err) => console.error(err)),
+  (lng) => i18nDamageReport.changeLanguage(lng).catch((err) => console.error(err)),
 );
 
 export default i18n;
