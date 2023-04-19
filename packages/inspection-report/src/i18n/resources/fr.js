2fc57df52f503d610c23cb13573365fe003231b2
Rohit Sharma <rohit.sharma@globant.com>
2023-04-19 12:17:59 +0530

    initialize locatization using i18n

---
+++ packages/inspection-report/src/i18n/resources/fr.js
@@ -0,0 +1,17 @@
+const fr = {
+  translation: {
+    damageReport: {
+      title: 'Damage Report...',
+      tabs: {
+        overview_tab: {
+          label: 'Overview...',
+        },
+        photos_tab: {
+          label: 'Photos.....',
+        },
+      },
+    },
+  },
+};
+
+export default fr;
