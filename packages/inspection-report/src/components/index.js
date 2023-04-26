2a5dcf1c0f88133629901268c8ba011de558428d
deepakglobant <123438709+deepakglobant@users.noreply.github.com>
2023-04-26 16:12:11 +0530

    feat: added gallery component (MN-253) (#476)
    * feat: added gallery component (MN-253)
    
    * fix: resolve pr comments
    
    * fix: resolve multilanguage issue in thumbnail
    
    ---------
    
    Co-authored-by: deepakglobant <deepak.chaudhary@globanmt.com>

--- packages/inspection-report/src/components/index.js
+++ packages/inspection-report/src/components/index.js
@@ -1,2 +1,3 @@
-export { default as DamageReport } from './DamageReport';
 export { default as CarView360 } from './CarView360';
+export { default as DamageReport } from './DamageReport';
+export { default as Gallery } from './Gallery';
---
+++ packages/inspection-report/src/mock.js
@@ -0,0 +1,34 @@
+export const pictureList = [
+  {
+    url: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+    label: {
+      fr: 'Hood',
+      en: 'Hood',
+    },
+  },
+  {
+    url: '',
+    label: {
+      fr: 'Front Door Right',
+      en: 'Front Door Right',
+    },
+  },
+  {
+    url: '',
+    label: {
+      fr: 'Rear Fender Left',
+      en: 'Rear Fender Left',
+    },
+  },
+  {
+    url: '',
+    label: {
+      fr: 'Front Fender Right',
+      en: 'Front Fender Right',
+    },
+  },
+];
+
+export default {
+  pictureList,
+};
