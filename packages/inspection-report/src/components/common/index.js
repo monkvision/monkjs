aa4bcefdd20387bbc75c44a563c7730c5be7e6b8
deepakglobant <deepak.chaudhary@globanmt.com>
2023-04-27 17:06:08 +0530

    feat: manipulate big modal (MN-257)

--- packages/inspection-report/src/components/common/index.js
+++ packages/inspection-report/src/components/common/index.js
@@ -1,5 +1,9 @@
+import DamageManipulator from './DamageManipulator';
 import IconButton from './IconButton';
+import SwitchButton from './SwitchButton';
 
 export {
+  DamageManipulator,
   IconButton,
+  SwitchButton,
 };
