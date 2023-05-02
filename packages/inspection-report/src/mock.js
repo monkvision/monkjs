72a1d82a45a6e66b8569e28bf86885e11793092d
deepakglobant <123438709+deepakglobant@users.noreply.github.com>
2023-05-02 15:54:00 +0530

    feat: manipulate big modal (MN-257) (#484)
    * feat: manipulate big modal (MN-257)
    
    * feat: implement big modal with small modal
    
    * update the code as per PR feedback and also add animation for sliding when user release the touch
    
    ---------
    
    Co-authored-by: deepakglobant <deepak.chaudhary@globanmt.com>
    Co-authored-by: Rohit Sharma <rohit.sharma@globant.com>

--- packages/inspection-report/src/mock.js
+++ packages/inspection-report/src/mock.js
@@ -1,4 +1,4 @@
-export const pictureList = [
+export const galleryList = [
   {
     url: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
     label: {
@@ -29,6 +29,22 @@ export const pictureList = [
   },
 ];
 
+export const pictureList = [
+  {
+    url: 'https://images.unsplash.com/photo-1583121274602-3e2820c69888?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8',
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1552519507-da3b142c6e3d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8',
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1494976388531-d1058494cdd8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8',
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1605559424843-9e4c228bf1c2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8',
+  },
+];
+
 export default {
+  galleryList,
   pictureList,
 };
