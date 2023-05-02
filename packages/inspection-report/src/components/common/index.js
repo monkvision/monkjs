36b4a45750077ca328b8475d8bb3fbc5e6c95c8a
deepakglobant <123438709+deepakglobant@users.noreply.github.com>
2023-05-02 15:54:00 +0530

    feat: manipulate big modal (MN-257) (#484)
    * feat: manipulate big modal (MN-257)
    
    * feat: implement big modal with small modal
    
    * update the code as per PR feedback and also add animation for sliding when user release the touch
    
    ---------
    
    Co-authored-by: deepakglobant <deepak.chaudhary@globanmt.com>
    Co-authored-by: Rohit Sharma <rohit.sharma@globant.com>

--- packages/inspection-report/src/components/common/index.js
+++ packages/inspection-report/src/components/common/index.js
@@ -1,8 +1,10 @@
+import DamageManipulator from './DamageManipulator';
 import IconButton from './IconButton';
 import TextButton from './TextButton';
 import SwitchButton from './SwitchButton';
 
 export {
+  DamageManipulator,
   IconButton,
   TextButton,
   SwitchButton,
