31be45dfa6f50b8d99adbd2d157545c680174110
ksenia_buntova <50693668+KseniaBuntova@users.noreply.github.com>
2023-08-03 15:24:55 +0300

    Fix enlarged SVGs and damage part selector (#9)

--- packages/camera/src/components/AddDamageModal/PartSelector.js
+++ packages/camera/src/components/AddDamageModal/PartSelector.js
@@ -22,7 +22,39 @@ const styles = StyleSheet.create({
 
 export default function PartSelector({ orientation, togglePart, isPartSelected, vehicleType }) {
   const { height } = useWindowDimensions();
-  const wireframeXML = useWireframe({ orientation, vehicleType });
+  let wireframeXML = useWireframe({ orientation, vehicleType });
+  if (Platform.OS !== 'web') {
+    const svgStyles = wireframeXML.substring(wireframeXML.indexOf('<style>') + '<style>'.length + 1, wireframeXML.indexOf('</style>'));
+    const elements = svgStyles.split('.');
+    const styleDict = {};
+    let classNames = [];
+    elements.forEach((element) => {
+      if (element.includes('{')) {
+        const style = element.substring(element.indexOf('{') + 1, element.indexOf('}'));
+        classNames.push(element.substring(0, element.indexOf('{')).replace(/[,.]/g, ''));
+        classNames.forEach((name) => {
+          const prev = styleDict[name] ? `${styleDict[name]};` : '';
+          styleDict[name] = `${prev}${style};`;
+        });
+        classNames = [];
+      } else {
+        classNames.push(element.replace(/[,.]/g, ''));
+      }
+    });
+
+    Object.keys(styleDict).forEach((key) => {
+      const newValue = styleDict[key].split(';').filter((value) => value.length !== 0).map((element) => {
+        const separatorIndex = element.indexOf(':');
+        const value = element.substring(separatorIndex + 1, element.length);
+        const name = element.substring(0, separatorIndex);
+        return `${name} = "${value}"`;
+      }).reduce(
+        (accumulator, currentValue) => `${accumulator} ${currentValue}`,
+        '',
+      );
+      wireframeXML = wireframeXML.replaceAll(`${key}"`, `${key}" ${newValue}`);
+    });
+  }
   const doc = useXMLParser(wireframeXML);
 
   const containerHeight = useMemo(
@@ -36,7 +68,7 @@ export default function PartSelector({ orientation, togglePart, isPartSelected, 
     if (Platform.OS === 'web') {
       svg = doc.children[0];
     } else {
-      svg = doc.childNodes[1];
+      svg = doc.childNodes[1] ?? doc.childNodes[0];
     }
     if (svg.tagName !== 'svg') {
       throw new Error('Invalid part selector SVG format: expected <svg> tag as the first children of XML document');
--- packages/camera/src/components/Overlay/index.native.js
+++ packages/camera/src/components/Overlay/index.native.js
@@ -3,9 +3,23 @@ import PropTypes from 'prop-types';
 import { SvgCss } from 'react-native-svg';
 
 export default function Overlay({ label, svg, ...passThoughProps }) {
+  let newSVG = svg;
+  if (!svg.includes('viewBox')) {
+    const widthParam = 'width="';
+    const widthStart = svg.indexOf(widthParam) + widthParam.length;
+    const widthEnd = widthStart + svg.substring(widthStart).indexOf('"');
+    const width = svg.substring(widthStart, widthEnd);
+
+    const heighParam = 'height="';
+    const heightStart = svg.indexOf(heighParam) + heighParam.length;
+    const heightEnd = heightStart + svg.substring(heightStart).indexOf('"');
+    const height = svg.substring(heightStart, heightEnd);
+
+    newSVG = svg.replace(svg.substring(svg.indexOf(widthParam), heightEnd + 1), `x="0" y="0" viewBox="0 0 ${width} ${height}"`);
+  }
   return (
     <SvgCss
-      xml={svg}
+      xml={newSVG}
       width="100%"
       height="100%"
       accessibilityLabel={`Overlay ${label}`}
