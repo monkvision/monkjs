9814b68b8267fc0eb76d5004b39c136caddbdfad
deepakglobant <123438709+deepakglobant@users.noreply.github.com>
2023-09-18 17:51:51 +0530

    fix: make add damage overlay responsive (MN-245) (#567)
    Co-authored-by: deepakglobant <deepak.chaudhary@globanmt.com>

--- packages/camera/src/components/AddDamageOverlay/index.js
+++ packages/camera/src/components/AddDamageOverlay/index.js
@@ -3,73 +3,37 @@ import React, { useMemo } from 'react';
 import { StyleSheet, useWindowDimensions, View } from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    right: 0,
-    left: 0,
-  },
   overlay: {
-    position: 'absolute',
-    backgroundColor: '#00000080',
+    position: 'fixed',
+    margin: 'auto',
+    left: 0,
+    right: 0,
+    bottom: 0,
+    top: 0,
+    boxShadow: '0 0 0 100pc #00000080',
   },
 });
 
 export default function AddDamageOverlay({ innerWidth, innerHeight }) {
   const { width, height } = useWindowDimensions();
   const effectiveInnerHeight = useMemo(
-    () => (innerHeight > height * 0.75 ? height * 0.75 : innerHeight),
+    () => (innerHeight > height * 0.75 ? innerHeight : height * 0.75),
     [height, innerHeight],
   );
   const effectiveInnerWidth = useMemo(
-    () => (innerWidth > width * 0.68 ? width * 0.68 : innerWidth),
-    [height, innerWidth],
+    () => (innerWidth > width * 0.68 ? innerWidth : width * 0.68),
+    [width, innerWidth],
   );
 
   return (
-    <>
-      <View style={[
-        styles.overlay,
-        {
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: effectiveInnerHeight + (height - effectiveInnerHeight) / 2,
-        },
-      ]}
-      />
-      <View style={[
-        styles.overlay,
-        {
-          top: effectiveInnerHeight + (height - effectiveInnerHeight) / 2,
-          left: 0,
-          right: 0,
-          bottom: 0,
-        },
-      ]}
-      />
-      <View style={[
-        styles.overlay,
-        {
-          top: (height - effectiveInnerHeight) / 2,
-          left: 0,
-          right: effectiveInnerWidth + (width - effectiveInnerWidth) / 2,
-          bottom: (height - effectiveInnerHeight) / 2,
-        },
-      ]}
-      />
-      <View style={[
-        styles.overlay,
-        {
-          top: (height - effectiveInnerHeight) / 2,
-          left: effectiveInnerWidth + (width - effectiveInnerWidth) / 2,
-          right: 0,
-          bottom: (height - effectiveInnerHeight) / 2,
-        },
-      ]}
-      />
-    </>
+    <View style={[
+      styles.overlay,
+      {
+        height: effectiveInnerHeight - (height - effectiveInnerHeight) / 2,
+        width: effectiveInnerWidth - (width - effectiveInnerWidth) / 2,
+      },
+    ]}
+    />
   );
 }
 
@@ -80,5 +44,5 @@ AddDamageOverlay.propTypes = {
 
 AddDamageOverlay.defaultProps = {
   innerHeight: 300,
-  innerWidth: 450,
+  innerWidth: 400,
 };
