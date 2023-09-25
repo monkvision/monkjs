a46b3e76eb49790dc795552b77938ef4d7da9d16
deepakglobant <123438709+deepakglobant@users.noreply.github.com>
2023-09-25 15:10:39 +0530

    feat: added useDesktopMode hook (MN-354) (#574)
    Co-authored-by: deepakglobant <deepak.chaudhary@globanmt.com>

--- packages/inspection-report/src/hooks/index.js
+++ packages/inspection-report/src/hooks/index.js
@@ -1,3 +1,4 @@
 // eslint-disable-next-line import/prefer-default-export
+export { default as useDesktopMode } from './useDesktopMode';
 export { default as useOrientation, ORIENTATION_MODE } from './useOrientation';
 export * from './useCurrency';
