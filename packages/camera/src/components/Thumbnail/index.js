1cf11471322548db9a57c5b89b02bb1f92a4b67b
deepakglobant <deepak.chaudhary@globant.com>
2023-10-18 15:54:25 +0530

    feat: allow retaking pictures (MN-366)

--- packages/camera/src/components/Thumbnail/index.js
+++ packages/camera/src/components/Thumbnail/index.js
@@ -14,6 +14,7 @@ const styles = StyleSheet.create({
   root: {
     display: 'flex',
     justifyContent: 'center',
+    cursor: 'pointer',
     width: LENGTH,
     height: LENGTH,
     margin: MARGIN,
