929739ca3676655a6dbf0297107a6027ada054b6
deepakglobant <deepak.chaudhary@globant.com>
2023-10-23 14:58:20 +0530

    fix: resolved current picture id if clicked again

--- packages/camera/src/components/Capture/capture.js
+++ packages/camera/src/components/Capture/capture.js
@@ -462,7 +462,9 @@ const Capture = forwardRef(({
   }, [sights.dispatch, sightBeforeRetake, onPictureTaken]);
 
   const handleSightPressed = useCallback((id) => {
-    setSightBeforeRetake(id);
+    if (!sightBeforeRetake) {
+      setSightBeforeRetake(id);
+    }
   }, [sightBeforeRetake]);
 
   // END HANDLERS //
