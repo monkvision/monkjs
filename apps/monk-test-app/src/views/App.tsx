d8795dad7cfd066ee889c4a20e28ecd9b25d4624
souyahia <souyahia@acvauctions.com>
2023-11-17 11:26:35 +0100

    Fixed the monitoring adapter function bindings

--- apps/monk-test-app/src/views/App.tsx
+++ apps/monk-test-app/src/views/App.tsx
@@ -2,11 +2,12 @@ import { i18nCamera } from '@monkvision/camera-web';
 import { useI18nLink } from '@monkvision/common';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { TestView } from './TestView';
+import { CameraView } from './CameraView';
+// import { TestView } from './TestView';
 
 export function App() {
   const { i18n } = useTranslation();
   useI18nLink(i18n, [i18nCamera]);
 
-  return <TestView />;
+  return <CameraView />;
 }
