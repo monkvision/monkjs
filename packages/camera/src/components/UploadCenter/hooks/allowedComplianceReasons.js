acdb1fd6373ba83b52695d7c72d72caf918c628b
Samy Ouyahia <103439265+souyahia-monk@users.noreply.github.com>
2023-11-30 14:23:25 +0100

    Removed unwanted compliances (#627)
    Co-authored-by: souyahia-monk <samy.ouyahia@monkvision.ai>

---
+++ packages/camera/src/components/UploadCenter/hooks/allowedComplianceReasons.js
@@ -0,0 +1,48 @@
+const ALLOWED_COMPLIANCE_REASONS = [
+  'LOW_RESOLUTION',
+  'BLURRINESS',
+  'UNDEREXPOSURE',
+  'OVEREXPOSURE',
+  'LENS_FLARE',
+  'DIRTINESS',
+  'SNOWNESS',
+  'WETNESS',
+  'REFLECTIONS',
+];
+
+export default function filterUnwantedComplianceReasons(reasons) {
+  if (!Array.isArray(reasons)) {
+    return reasons;
+  }
+  return reasons.filter(
+    (reason) => ALLOWED_COMPLIANCE_REASONS
+      .findIndex((allowedReason) => reason.startsWith(allowedReason)) !== -1,
+  );
+}
+
+/*
+ * For reference, here is the complete list of compliances reasons known to this date.
+ *   - Samy 22/11/23
+ * OTHER
+ * LOW_RESOLUTION
+ * BLURRINESS
+ * UNDEREXPOSURE
+ * OVEREXPOSURE
+ * LENS_FLARE
+ * DIRTINESS
+ * SNOWNESS
+ * WETNESS
+ * REFLECTIONS
+ * UNKNOWN_SIGHT
+ * UNKNOWN_VIEWPOINT
+ * NO_VEHICLE
+ * WRONG_ANGLE
+ * WRONG_CENTER_PART
+ * MISSING_PARTS
+ * HIDDEN_PARTS
+ * TOO_ZOOMED
+ * NOT_ZOOMED_ENOUGH
+ * INTERIOR_NOT_SUPPORTED
+ * MISSING
+ * LOW_QUALITY
+ */
