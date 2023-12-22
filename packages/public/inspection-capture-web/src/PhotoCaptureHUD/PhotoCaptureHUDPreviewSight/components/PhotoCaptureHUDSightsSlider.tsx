1ea642d5617f5412acd5f99aca9a47ce2d13bea2
David_LY <v-dly@acvauctions.com>
2023-12-22 12:28:03 +0100

    sight centered in slider when selected

--- packages/public/inspection-capture-web/src/PhotoCaptureHUD/PhotoCaptureHUDPreviewSight/components/PhotoCaptureHUDSightsSlider.tsx
+++ packages/public/inspection-capture-web/src/PhotoCaptureHUD/PhotoCaptureHUDPreviewSight/components/PhotoCaptureHUDSightsSlider.tsx
@@ -2,6 +2,7 @@ import { Button } from '@monkvision/common-ui-web';
 import { Sight } from '@monkvision/types';
 import { useSightLabel } from '@monkvision/common';
 import { labels } from '@monkvision/sights';
+import { useRef } from 'react';
 import { usePhotoCaptureHUDPreview } from '../hook';
 
 export interface PhotoCaptureHUDSightsSliderProps {
@@ -15,17 +16,26 @@ export function PhotoCaptureHUDSightsSlider({
   currentSight,
   onSightSelected = () => {},
 }: PhotoCaptureHUDSightsSliderProps) {
+  const ref = useRef<HTMLDivElement>(null);
   const { label } = useSightLabel({ labels });
   const style = usePhotoCaptureHUDPreview();
 
+  const onScrollToSelected = (index: number): void => {
+    if (ref.current && ref.current.children.length > index)
+      ref.current.children[index].scrollIntoView({ behavior: 'smooth', inline: 'center' });
+  };
+
   return (
-    <div style={style.slider}>
+    <div style={style.slider} ref={ref}>
       {sights?.map((sight, index) => (
         <Button
           style={style.labelButton}
           key={index}
           primaryColor={sight.id === currentSight ? 'primary-base' : 'secondary-xdark'}
-          onClick={() => onSightSelected(sight)}
+          onClick={() => {
+            onSightSelected(sight);
+            onScrollToSelected(index);
+          }}
           data-testid={`sight-btn-${index}`}
         >
           {label(sight)}
