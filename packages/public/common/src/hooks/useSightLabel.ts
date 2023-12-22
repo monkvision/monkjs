7c3f7ca5e58cbffd5c7b164269fb6b3845b3619a
David_LY <v-dly@acvauctions.com>
2023-12-22 20:12:18 +0100

    TSDoc useSightLabel

--- packages/public/common/src/hooks/useSightLabel.ts
+++ packages/public/common/src/hooks/useSightLabel.ts
@@ -1,14 +1,25 @@
 import { LabelDictionary, Sight } from '@monkvision/types';
 import { useObjectTranslation } from './useObjectTranslation';
 
+/**
+ * The result of the useSightLabel. It contains a function which takes a Sight object and return the
+ * translated label sync with the actual selected language.
+ */
 export interface UseSightLabelResult {
   label: (sight: Sight) => string;
 }
 
 export interface UseSightLabelParams {
+  /**
+   * Function translating a Sight object into a translated label sync with the actual selected language.
+   * @param obj
+   */
   labels: LabelDictionary;
 }
 
+/**
+ * Custom hook used to get the label with the actual selected language.
+ */
 export function useSightLabel({ labels }: UseSightLabelParams): UseSightLabelResult {
   const { tObj } = useObjectTranslation();
   return {
