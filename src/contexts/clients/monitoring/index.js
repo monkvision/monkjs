7daa9746d5205e00f0eb146213e60013bbbfa02a
deepakglobant <deepak.chaudhary@globant.com>
2024-01-04 10:39:23 +0530

    feat: add sentry for algodriven (MN-444)

--- src/contexts/clients/monitoring/index.js
+++ src/contexts/clients/monitoring/index.js
@@ -3,12 +3,15 @@ import defaultConfig from './default';
 import catConfig from './cat';
 import fastbackConfig from './fastback';
 import alphaConfig from './alpha';
+import algoDrivenConfig from './algodriven';
 
 const ClientMonitoring = {
   [Clients.DEFAULT]: defaultConfig,
   [Clients.CAT]: catConfig,
   [Clients.FASTBACK]: fastbackConfig,
   [Clients.ALPHA]: alphaConfig,
+  [Clients.ALGODRIVEN_CAPTURE]: algoDrivenConfig,
+  [Clients.ALGODRIVEN_REPORT]: algoDrivenConfig,
 };
 
 export default ClientMonitoring;
