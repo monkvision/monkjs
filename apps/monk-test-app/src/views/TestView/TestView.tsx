5f453c6b04f6879f834d196ba4a78b0f37a574fd
souyahia <souyahia@acvauctions.com>
2024-03-03 22:43:30 +0100

    Always display camera preview with highest res possible

--- apps/monk-test-app/src/views/TestView/TestView.tsx
+++ apps/monk-test-app/src/views/TestView/TestView.tsx
@@ -1,52 +1,39 @@
-import React, { useState } from 'react';
-import { PhotoCapture } from '@monkvision/inspection-capture-web';
-import { sights } from '@monkvision/sights';
+import { Camera, CameraResolution, MonkPicture, SimpleCameraHUD } from '@monkvision/camera-web';
 import './TestView.css';
-
-const captureSights = [
-  sights['haccord-8YjMcu0D'],
-  sights['haccord-DUPnw5jj'],
-  sights['haccord-hsCc_Nct'],
-  sights['haccord-GQcZz48C'],
-  sights['haccord-QKfhXU7o'],
-  sights['haccord-mdZ7optI'],
-  sights['haccord-bSAv3Hrj'],
-  sights['haccord-W-Bn3bU1'],
-  sights['haccord-GdWvsqrm'],
-  sights['haccord-ps7cWy6K'],
-  sights['haccord-Jq65fyD4'],
-  sights['haccord-OXYy5gET'],
-  sights['haccord-5LlCuIfL'],
-  sights['haccord-Gtt0JNQl'],
-  sights['haccord-cXSAj2ez'],
-  sights['haccord-KN23XXkX'],
-  sights['haccord-Z84erkMb'],
-];
-
-const inspectionId = 'b072ff42-6244-ef3b-b018-5d3d6562c1bd';
-
-const apiConfig = {
-  apiDomain: 'api.preview.monk.ai/v1',
-  authToken:
-    'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjNLUnpaNm01WDFzOWFBZWRudnBrWSJ9.eyJpc3MiOiJodHRwczovL2lkcC5wcmV2aWV3Lm1vbmsuYWkvIiwic3ViIjoiZ29vZ2xlLW9hdXRoMnwxMDY5MzYxMTEwMDU4MDYxODA1NTYiLCJhdWQiOlsiaHR0cHM6Ly9hcGkubW9uay5haS92MS8iLCJodHRwczovL21vbmstcHJldmlldy5ldS5hdXRoMC5jb20vdXNlcmluZm8iXSwiaWF0IjoxNzA3NDcxNzU5LCJleHAiOjE3MDc0Nzg5NTksImF6cCI6InNvWjdQMmM2YjlJNWphclFvUnJoaDg3eDlUcE9TYUduIiwic2NvcGUiOiJvcGVuaWQgcHJvZmlsZSBlbWFpbCIsInBlcm1pc3Npb25zIjpbIm1vbmtfY29yZV9hcGk6Y29tcGxpYW5jZXMiLCJtb25rX2NvcmVfYXBpOmRhbWFnZV9kZXRlY3Rpb24iLCJtb25rX2NvcmVfYXBpOmRhc2hib2FyZF9vY3IiLCJtb25rX2NvcmVfYXBpOmltYWdlc19vY3IiLCJtb25rX2NvcmVfYXBpOmluc3BlY3Rpb25zOmNyZWF0ZSIsIm1vbmtfY29yZV9hcGk6aW5zcGVjdGlvbnM6ZGVsZXRlIiwibW9ua19jb3JlX2FwaTppbnNwZWN0aW9uczpkZWxldGVfYWxsIiwibW9ua19jb3JlX2FwaTppbnNwZWN0aW9uczpkZWxldGVfb3JnYW5pemF0aW9uIiwibW9ua19jb3JlX2FwaTppbnNwZWN0aW9uczpyZWFkIiwibW9ua19jb3JlX2FwaTppbnNwZWN0aW9uczpyZWFkX2FsbCIsIm1vbmtfY29yZV9hcGk6aW5zcGVjdGlvbnM6cmVhZF9vcmdhbml6YXRpb24iLCJtb25rX2NvcmVfYXBpOmluc3BlY3Rpb25zOnVwZGF0ZSIsIm1vbmtfY29yZV9hcGk6aW5zcGVjdGlvbnM6dXBkYXRlX2FsbCIsIm1vbmtfY29yZV9hcGk6aW5zcGVjdGlvbnM6dXBkYXRlX29yZ2FuaXphdGlvbiIsIm1vbmtfY29yZV9hcGk6aW5zcGVjdGlvbnM6d3JpdGUiLCJtb25rX2NvcmVfYXBpOmluc3BlY3Rpb25zOndyaXRlX2FsbCIsIm1vbmtfY29yZV9hcGk6aW5zcGVjdGlvbnM6d3JpdGVfb3JnYW5pemF0aW9uIiwibW9ua19jb3JlX2FwaTpyZXBhaXJfZXN0aW1hdGUiLCJtb25rX2NvcmVfYXBpOnVzZXJzOnJlYWQiLCJtb25rX2NvcmVfYXBpOnVzZXJzOnJlYWRfYWxsIiwibW9ua19jb3JlX2FwaTp1c2VyczpyZWFkX29yZ2FuaXphdGlvbiIsIm1vbmtfY29yZV9hcGk6dXNlcnM6dXBkYXRlIiwibW9ua19jb3JlX2FwaTp1c2Vyczp1cGRhdGVfYWxsIiwibW9ua19jb3JlX2FwaTp1c2Vyczp1cGRhdGVfb3JnYW5pemF0aW9uIiwibW9ua19jb3JlX2FwaTp1c2Vyczp3cml0ZSIsIm1vbmtfY29yZV9hcGk6dXNlcnM6d3JpdGVfYWxsIiwibW9ua19jb3JlX2FwaTp1c2Vyczp3cml0ZV9vcmdhbml6YXRpb24iLCJtb25rX2NvcmVfYXBpOndoZWVsX2FuYWx5c2lzIl19.m38G5NaCD_pcGptJdLPa_TVtD08yRY9qdp-5pCELd8Ekzma0kCWMctwHvlrv2OsjynlXyAutIK2uhDMrdLdnPk_6bU4rZheej2s0obXaXqZCUTbUOh8scL-81tbH_ZlKN3oSXfqUVMnwvpa1bnXZHmjeHi2e3bhvjxW-Jg5DrBB9gNfstxK0hugrlxtNL96y6ImEITOxOMEbURYGwOLQQtLkRqFo7AeZCu-_w6UbtFZfLJc5FlsWpTKy7I3_xMynzDGGaADRQeyazL_7DfemCb_VPXkR9aV67tF4pt6jevCetYI5CfN5X2JJrp7XNES_M2d7wGdJl5C6lbBPs3n3SA',
-};
+import { useState } from 'react';
 
 export function TestView() {
-  const [complete, setComplete] = useState(false);
+  const [resolution, setResolution] = useState(CameraResolution.UHD_4K);
+
+  const handlePictureTaken = (picture: MonkPicture) => {
+    const link = document.createElement('a');
+    link.href = picture.uri;
+    const now = new Date();
+    link.download = `pic-${resolution}-${now.getHours()}-${now.getMinutes()}-${now.getSeconds()}.jpg`;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
 
   return (
     <div className='test-view-container'>
-      {complete ? (
-        'Inspection Complete!'
-      ) : (
-        <PhotoCapture
-          sights={captureSights}
-          inspectionId={inspectionId}
-          apiConfig={apiConfig}
-          onComplete={() => setComplete(true)}
-          onClose={() => console.log('coucou')}
-        />
-      )}
+      <Camera
+        HUDComponent={SimpleCameraHUD}
+        resolution={resolution}
+        onPictureTaken={handlePictureTaken}
+      />
+      <div className='select-container'>
+        <select
+          value={resolution}
+          onChange={(e) => setResolution(e.target.value as CameraResolution)}
+        >
+          {Object.values(CameraResolution).map((res) => (
+            <option key={res} value={res}>
+              {res}
+            </option>
+          ))}
+        </select>
+      </div>
     </div>
   );
 }
--- packages/public/camera-web/test/Camera/hooks/useCameraCanvas.test.ts
+++ packages/public/camera-web/test/Camera/hooks/useCameraCanvas.test.ts
@@ -1,6 +1,22 @@
+/* eslint-disable jest/valid-title */
+import { getResolutionDimensions } from '../../../src/Camera/hooks/utils';
+
+jest.mock('../../../src/Camera/hooks/utils', () => ({
+  ...jest.requireActual('../../../src/Camera/hooks/utils'),
+  getResolutionDimensions: jest.fn(() => ({ width: 1, height: 1 })),
+}));
+
 import { renderHook } from '@testing-library/react-hooks';
 import React from 'react';
-import { useCameraCanvas } from '../../../src/Camera/hooks';
+import { CameraCanvasConfig, CameraResolution, useCameraCanvas } from '../../../src/Camera/hooks';
+
+function createProps(): CameraCanvasConfig {
+  return {
+    resolution: CameraResolution.HD_720P,
+    streamDimensions: { width: 4567, height: 1234 },
+    allowImageUpscaling: false,
+  };
+}
 
 describe('useCameraCanvas hook', () => {
   afterEach(() => {
@@ -10,22 +26,97 @@ describe('useCameraCanvas hook', () => {
   it('should return a ref object', () => {
     const useRefSpy = jest.spyOn(React, 'useRef');
 
-    const { result, unmount } = renderHook(useCameraCanvas, { initialProps: { dimensions: null } });
+    const { result, unmount } = renderHook(useCameraCanvas, { initialProps: createProps() });
 
     expect(useRefSpy).toHaveBeenCalledWith(null);
     expect(result.current.ref).toBe(useRefSpy.mock.results[0].value);
     unmount();
   });
 
-  it('should update the dimensions of the canvas when the dimensions change', () => {
-    const ref = { current: { width: 0, height: 0 } };
-    const dimensions = { width: 99, height: 112 };
+  it('should not update the dimensions of the canvas when the stream dimensions are null', () => {
+    const width = 12;
+    const height = 24;
+    const ref = { current: { width, height } };
+    const initialProps = { ...createProps(), streamDimensions: null };
     jest.spyOn(React, 'useRef').mockImplementation(() => ref);
 
-    const { unmount } = renderHook(useCameraCanvas, { initialProps: { dimensions } });
+    const { result, unmount } = renderHook(useCameraCanvas, { initialProps });
 
-    expect(ref.current.width).toEqual(dimensions.width);
-    expect(ref.current.height).toEqual(dimensions.height);
+    expect(ref.current.width).toEqual(width);
+    expect(ref.current.height).toEqual(height);
+    expect(result.current.dimensions).toBeNull();
     unmount();
+  });
+
+  [
+    {
+      title:
+        'should handle the case where asked resolution is bigger than stream resolution but upscaling is disabled',
+      streamDimensions: { width: 500, height: 500 },
+      askedDimensions: { width: 5000, height: 8000 },
+      allowImageUpscaling: false,
+      expected: { width: 500, height: 500 },
+    },
+    {
+      title:
+        'should handle the case where asked resolution is bigger than stream resolution and upscaling is enabled',
+      streamDimensions: { width: 500, height: 500 },
+      askedDimensions: { width: 5000, height: 5000 },
+      allowImageUpscaling: true,
+      expected: { width: 5000, height: 5000 },
+    },
+    {
+      title: 'should handle the case where asked resolution is smaller than the stream resolution',
+      streamDimensions: { width: 5000, height: 5000 },
+      askedDimensions: { width: 500, height: 500 },
+      allowImageUpscaling: false,
+      expected: { width: 500, height: 500 },
+    },
+    {
+      title: 'should handle smaller resolution and wrong ratio with fit to width',
+      streamDimensions: { width: 5000, height: 5000 },
+      askedDimensions: { width: 500, height: 800 },
+      allowImageUpscaling: false,
+      expected: { width: 500, height: 500 },
+    },
+    {
+      title: 'should handle smaller resolution and wrong ratio with fit to height',
+      streamDimensions: { width: 5000, height: 5000 },
+      askedDimensions: { width: 800, height: 400 },
+      allowImageUpscaling: false,
+      expected: { width: 400, height: 400 },
+    },
+    {
+      title: 'should handle bigger resolution and wrong ratio with upscaling',
+      streamDimensions: { width: 500, height: 500 },
+      askedDimensions: { width: 8000, height: 5000 },
+      allowImageUpscaling: true,
+      expected: { width: 5000, height: 5000 },
+    },
+  ].forEach((params) => {
+    it(params.title, () => {
+      const ref = { current: { width: 0, height: 0 } };
+      const initialProps = {
+        resolution: CameraResolution.QNHD_180P,
+        streamDimensions: params.streamDimensions,
+        allowImageUpscaling: params.allowImageUpscaling,
+      };
+      jest.spyOn(React, 'useRef').mockImplementation(() => ref);
+      (getResolutionDimensions as jest.Mock).mockImplementationOnce(() => params.askedDimensions);
+
+      const { result, unmount } = renderHook(useCameraCanvas, { initialProps });
+
+      expect(getResolutionDimensions).toHaveBeenCalledWith(
+        initialProps.resolution,
+        params.streamDimensions.width < params.streamDimensions.height,
+      );
+      expect(ref.current.width).toEqual(params.expected.width);
+      expect(ref.current.height).toEqual(params.expected.height);
+      expect(result.current.dimensions).toEqual({
+        width: params.expected.width,
+        height: params.expected.height,
+      });
+      unmount();
+    });
   });
 });
