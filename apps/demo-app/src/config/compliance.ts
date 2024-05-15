b3872f9d44fa347135f1f4ca1e890088571a6388
Samy Ouyahia <103439265+souyahia-monk@users.noreply.github.com>
2024-05-15 10:13:02 +0200

    Disable the WRONG_ANGLE compliance issue in demo and drive apps (#742)
    * Disable the WRONG_ANGLE compliance issue in demo and drive apps
    
    * Changed the default app titles

---
+++ apps/demo-app/src/config/compliance.ts
@@ -0,0 +1,5 @@
+import { ComplianceIssue, DEFAULT_COMPLIANCE_ISSUES } from '@monkvision/types';
+
+export const complianceIssues: ComplianceIssue[] = DEFAULT_COMPLIANCE_ISSUES.filter(
+  (issue) => issue !== ComplianceIssue.WRONG_ANGLE,
+);
--- apps/drive-app/src/config/compliance.ts
+++ apps/drive-app/src/config/compliance.ts
@@ -1,7 +1,15 @@
-import { ComplianceIssue, IQA_COMPLIANCE_ISSUES } from '@monkvision/types';
+import {
+  ComplianceIssue,
+  DEFAULT_COMPLIANCE_ISSUES,
+  IQA_COMPLIANCE_ISSUES,
+} from '@monkvision/types';
 
 export const complianceIssuesPerSight: Record<string, ComplianceIssue[]> = {
   'all-IqwSM3': IQA_COMPLIANCE_ISSUES,
   'all-rSvk2C': IQA_COMPLIANCE_ISSUES,
   'all-T4HrF8KA': IQA_COMPLIANCE_ISSUES,
 };
+
+export const complianceIssues: ComplianceIssue[] = DEFAULT_COMPLIANCE_ISSUES.filter(
+  (issue) => issue !== ComplianceIssue.WRONG_ANGLE,
+);
