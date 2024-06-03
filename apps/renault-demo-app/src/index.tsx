fb0db383a0d92bb1d0514b68e14c8da95cc9818c
Samy Ouyahia <103439265+souyahia-monk@users.noreply.github.com>
2024-06-03 10:40:07 +0200

    Re-enabled Sentry and Posthog (#770)

--- apps/renault-demo-app/src/index.tsx
+++ apps/renault-demo-app/src/index.tsx
@@ -1,22 +1,18 @@
 import ReactDOM from 'react-dom';
-import { DebugMonitoringAdapter, MonitoringProvider } from '@monkvision/monitoring';
-import { AnalyticsProvider, EmptyAnalyticsAdapter } from '@monkvision/analytics';
+import { MonitoringProvider } from '@monkvision/monitoring';
+import { AnalyticsProvider } from '@monkvision/analytics';
 import { Auth0Provider } from '@auth0/auth0-react';
 import { getEnvOrThrow, MonkThemeProvider } from '@monkvision/common';
-// import { sentryMonitoringAdapter } from './sentry';
-// import { posthogAnalyticsAdapter } from './posthog';
+import { sentryMonitoringAdapter } from './sentry';
+import { posthogAnalyticsAdapter } from './posthog';
 import { AppRouter } from './components';
 import { AppConfig } from './config';
 import './index.css';
 import './i18n';
 
 ReactDOM.render(
-  <MonitoringProvider
-    adapter={new DebugMonitoringAdapter({ showUnsupportedMethodWarnings: false })}
-  >
-    <AnalyticsProvider
-      adapter={new EmptyAnalyticsAdapter({ showUnsupportedMethodWarnings: false })}
-    >
+  <MonitoringProvider adapter={sentryMonitoringAdapter}>
+    <AnalyticsProvider adapter={posthogAnalyticsAdapter}>
       <Auth0Provider
         domain={getEnvOrThrow('REACT_APP_AUTH_DOMAIN')}
         clientId={getEnvOrThrow('REACT_APP_AUTH_CLIENT_ID')}
