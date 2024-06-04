75632eab4c3ad422de5d57840e3d286364c76e66
Samy Ouyahia <103439265+souyahia-monk@users.noreply.github.com>
2024-06-04 15:54:47 +0200

    Created lux demo app (#774)

---
+++ apps/lux-demo-app/src/index.tsx
@@ -0,0 +1,32 @@
+import ReactDOM from 'react-dom';
+import { MonitoringProvider } from '@monkvision/monitoring';
+import { AnalyticsProvider } from '@monkvision/analytics';
+import { Auth0Provider } from '@auth0/auth0-react';
+import { getEnvOrThrow, MonkThemeProvider } from '@monkvision/common';
+import { sentryMonitoringAdapter } from './sentry';
+import { posthogAnalyticsAdapter } from './posthog';
+import { AppRouter } from './components';
+import './index.css';
+import './i18n';
+import { AppConfig } from './config';
+
+ReactDOM.render(
+  <MonitoringProvider adapter={sentryMonitoringAdapter}>
+    <AnalyticsProvider adapter={posthogAnalyticsAdapter}>
+      <Auth0Provider
+        domain={getEnvOrThrow('REACT_APP_AUTH_DOMAIN')}
+        clientId={getEnvOrThrow('REACT_APP_AUTH_CLIENT_ID')}
+        authorizationParams={{
+          redirect_uri: window.location.origin,
+          audience: getEnvOrThrow('REACT_APP_AUTH_AUDIENCE'),
+          prompt: 'login',
+        }}
+      >
+        <MonkThemeProvider palette={AppConfig.palette}>
+          <AppRouter />
+        </MonkThemeProvider>
+      </Auth0Provider>
+    </AnalyticsProvider>
+  </MonitoringProvider>,
+  document.getElementById('root'),
+);
---
+++ apps/lux-demo-app/src/pages/index.ts
@@ -0,0 +1,5 @@
+export * from './pages';
+export * from './LoginPage';
+export * from './CreateInspectionPage';
+export * from './PhotoCapturePage';
+export * from './VehicleTypeSelectionPage';
