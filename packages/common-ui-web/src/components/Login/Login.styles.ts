a70e410770529544b3f52e882eb7b75bf25d97ff
arunachalam <aelango@acvauctions.com>
2024-06-06 16:18:27 +0530

    💄 make login error message look good on vertical

--- packages/common-ui-web/src/components/Login/Login.styles.ts
+++ packages/common-ui-web/src/components/Login/Login.styles.ts
@@ -7,7 +7,11 @@ export const styles: Styles = {
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
+    gap: '1.5em',
     alignItems: 'center',
+    textAlign: 'center',
+    maxWidth: '70%',
+    margin: 'auto',
   },
   errorMessage: {
     textAlign: 'center',
