2cb185257f63078d1447b899633323d32dad3e7b
Samy Ouyahia <souyahia@acvauctions.com>
2024-06-10 15:37:10 +0200

    Added a no cache URL param in the API request

--- packages/network/src/api/liveConfigs/requests.ts
+++ packages/network/src/api/liveConfigs/requests.ts
@@ -10,7 +10,7 @@ import { getDefaultOptions } from '../config';
 export async function getLiveConfig(id: string): Promise<LiveConfig> {
   const kyOptions = getDefaultOptions();
   const response = await ky.get(
-    `https://storage.googleapis.com/monk-front-public/live-configurations/${id}.json`,
+    `https://storage.googleapis.com/monk-front-public/live-configurations/${id}.json?nocache=${Date.now()}`,
     kyOptions,
   );
   return response.json<LiveConfig>();
