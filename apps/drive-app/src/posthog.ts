3791839f98db3494d7d103f6247d9c0c71128127
David LY <153214527+dlymonkai@users.noreply.github.com>
2024-06-18 11:55:06 +0200

    Add new posthog token to drive (#791)

--- apps/drive-app/src/posthog.ts
+++ apps/drive-app/src/posthog.ts
@@ -2,7 +2,7 @@ import { PosthogAnalyticsAdapter } from '@monkvision/posthog';
 import { getEnvOrThrow } from '@monkvision/common';
 
 export const posthogAnalyticsAdapter = new PosthogAnalyticsAdapter({
-  token: 'phc_9mKWu5rYzvrUT6Bo3bTzrclNa5sOILKthH9BA9sna0M',
+  token: 'phc_f1KJvZbcPCo8lsMBb63B6d9BxqKcjPdldFu5Nftywi1',
   api_host: 'https://eu.posthog.com',
   environnement: getEnvOrThrow('REACT_APP_ENVIRONMENT'),
   projectName: 'drive-app',
