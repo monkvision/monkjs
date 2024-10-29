006f3754349aa42cbd6087c5ebf18fdb25bc4a3f
David Ly <dly@acvauctions.com>
2024-10-29 11:10:07 +0100

    Fixed sentry DSN

--- apps/demo-app/src/sentry.ts
+++ apps/demo-app/src/sentry.ts
@@ -2,7 +2,7 @@ import { SentryMonitoringAdapter } from '@monkvision/sentry';
 import { getEnvOrThrow } from '@monkvision/common';
 
 export const sentryMonitoringAdapter = new SentryMonitoringAdapter({
-  dsn: 'https://74f50bfe6f11de7aefd54acfa5dfed96@o4505669501648896.ingest.us.sentry.io/4506863461662720',
+  dsn: getEnvOrThrow('REACT_APP_SENTRY_DSN'),
   environment: getEnvOrThrow('REACT_APP_ENVIRONMENT'),
   debug: process.env['REACT_APP_SENTRY_DEBUG'] === 'true',
   tracesSampleRate: 0.025,
