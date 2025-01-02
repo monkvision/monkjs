4a8be16441069195905405b351351735d5dec35e
David Ly <dly@acvauctions.com>
2025-01-02 16:09:56 +0100

    Added 'VehiclePartSelection' component

--- packages/common-ui-web/src/components/VehiclePartSelection/VehiclePartSelection.style.ts
+++ packages/common-ui-web/src/components/VehiclePartSelection/VehiclePartSelection.style.ts
@@ -1,12 +1,28 @@
 import { Styles } from '@monkvision/types';
 
+export const ICON_SIZE = 40;
+
 export const styles: Styles = {
   wrapper: {
-    position: 'absolute',
     display: 'flex',
-    justifyContent: 'center',
     alignItems: 'center',
-    inset: '0 0 0 0',
-    gap: '30px',
+    justifyContent: 'space-between',
+    height: '100%',
+    width: '100%',
+  },
+  wireframeContainer: {
+    height: '90%',
+    width: '100%',
+    position: 'absolute',
+    top: '50%',
+    transform: 'translateY(-50%)',
+  },
+  leftArrowContainer: {
+    display: 'flex',
+  },
+  spacer: { width: `${ICON_SIZE}px` },
+  rightArrow: {
+    position: 'absolute',
+    right: 0,
   },
 };
