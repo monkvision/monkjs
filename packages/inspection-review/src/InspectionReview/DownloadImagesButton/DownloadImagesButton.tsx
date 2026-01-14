f3cb144d4ccb6e556205b8b02238d74f19464797
Gabriel Tira <gtira@acvauctions.com>
2026-01-14 10:41:43 +0100

    Added optional onDownloadImages; minor fixes

--- packages/inspection-review/src/InspectionReview/DownloadImagesButton/DownloadImagesButton.tsx
+++ packages/inspection-review/src/InspectionReview/DownloadImagesButton/DownloadImagesButton.tsx
@@ -3,18 +3,19 @@ import { Button } from '@monkvision/common-ui-web';
 import { useMonkTheme } from '@monkvision/common';
 import { useDownloadImages } from './hooks/useDownloadImages';
 import { styles } from './DownloadImagesButton.styles';
+import { DownloadImagesButtonProps } from '../types/download-images.types';
 
 /**
  * The DownloadImagesButton component that provides a button to download images from the inspection review.
  */
-export function DownloadImagesButton() {
+export function DownloadImagesButton(props: DownloadImagesButtonProps) {
   const { palette } = useMonkTheme();
   const { t } = useTranslation();
-  const { loading, handleDownloadPictures } = useDownloadImages();
+  const { loading, handleDownloadImages } = useDownloadImages(props);
 
   return (
     <Button
-      onClick={handleDownloadPictures}
+      onClick={handleDownloadImages}
       icon='file-download'
       loading={loading}
       variant='text'
