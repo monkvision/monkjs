db1574fbef3b5a1fbff4d5606d530d3c7214eb99
Gabriel Tira <gtira@acvauctions.com>
2026-01-28 15:48:15 +0100

    Added styles for ExteriorDamage and Gallery

---
+++ packages/inspection-review/src/InspectionReview/ExteriorTab/AddExteriorDamage/AddExteriorDamage.styles.ts
@@ -0,0 +1,93 @@
+import { Styles } from '@monkvision/types';
+
+export const styles: Styles = {
+  container: {
+    alignSelf: 'end',
+    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+    borderRadius: '20px 20px 20px 20px',
+    top: '35px',
+    width: '100%',
+  },
+  title: {
+    padding: '16px',
+    display: 'flex',
+    justifyContent: 'center',
+    fontWeight: '700',
+    fontSize: '18px',
+  },
+  isDamageContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    flexWrap: 'wrap',
+    paddingBottom: '10px',
+  },
+  switchButtonContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    padding: '16px',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+  },
+  damagesColumnContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    flexWrap: 'wrap',
+    gap: '5px',
+  },
+  price: {
+    border: 0,
+    outlineWidth: 0,
+    width: '100%',
+    fontSize: '15px',
+    letterSpacing: '0.5px',
+    padding: '16px',
+  },
+  section: {
+    padding: '16px',
+  },
+  inputSectionContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: '100%',
+    justifyContent: 'space-between',
+  },
+  inputSection: {
+    display: 'flex',
+    flexDirection: 'row',
+    border: 'solid rgba(1,1,1,0.4)',
+    paddingRight: '10px',
+    margin: '0px 19px 0px 19px',
+  },
+  footerContainer: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  button: {
+    width: '200px',
+    padding: '10px 20px',
+    border: 'solid rgba(1, 1, 1, .2)',
+    cursor: 'pointer',
+    fontSize: '16px',
+    color: 'black',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: '20px',
+    borderRadius: '9px',
+  },
+  currency: {
+    alignSelf: 'center',
+    paddingLeft: '20px',
+    paddingRight: '5px',
+  },
+  cancel: {
+    background: 'white',
+  },
+};
---
+++ packages/inspection-review/src/InspectionReview/ReviewGallery/GalleryItem/GalleryItemCard.tsx
@@ -0,0 +1,42 @@
+import { useInteractiveStatus, useObjectTranslation } from '@monkvision/common';
+import { GalleryItem } from '../../types';
+import { useGalleryItemCardStyles } from './useGalleryItemCardStyles';
+
+/**
+ * Props accepted by the GalleryItemCard component.
+ */
+export interface GalleryItemProps {
+  /**
+   * The gallery item to be displayed.
+   */
+  item: GalleryItem;
+  /**
+   * Callback function invoked when the item is selected.
+   */
+  onSelectItem: (id: string) => void;
+}
+
+/**
+ * The GalleryItemCard component that displays an individual gallery item as a card.
+ */
+export function GalleryItemCard({ item, onSelectItem }: GalleryItemProps) {
+  const { tObj } = useObjectTranslation();
+  const { status, eventHandlers } = useInteractiveStatus();
+  const { cardStyle, previewStyle, labelStyle } = useGalleryItemCardStyles({
+    item,
+    status,
+    isDamaged: false,
+  });
+
+  return (
+    <button
+      key={item.image.id}
+      style={cardStyle}
+      onClick={() => onSelectItem(item.image.id)}
+      {...eventHandlers}
+    >
+      <div style={previewStyle} data-testid='preview' />
+      <div style={labelStyle}>{item.image.label ? tObj(item.image.label) : ''}</div>
+    </button>
+  );
+}
