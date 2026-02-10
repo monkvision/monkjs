a8662af412fed370b990608931c83874f12e4a21
Gabriel Tira <gtira@acvauctions.com>
2026-02-10 16:48:19 +0100

    Added more business rules for Exterior damages and gallery items filtering

--- packages/inspection-review/src/utils/galleryItems.utils.ts
+++ packages/inspection-review/src/utils/galleryItems.utils.ts
@@ -1,4 +1,5 @@
 import { Polygon } from '@monkvision/types';
+import { GalleryItem } from '../types';
 
 /**
  * Helper function to calculate the area of a polygon using the Shoelace formula.
@@ -17,4 +18,46 @@ export function calculatePolygonArea(polygon: Polygon): number {
     area += polygon[i][0] * polygon[j][1] - polygon[j][0] * polygon[i][1];
   }
   return Math.abs(area / 2);
+}
+
+/**
+ * Helper function to filter out duplicate gallery items based on their sight labels,
+ * keeping only the most recent item for each sight.
+ *
+ * Images `createdAt` being undefined are considered less recent than those with a defined `createdAt`.
+ */
+export function filterDuplicateSights(items: GalleryItem[]): GalleryItem[] {
+  const sightLabelMap = new Map<string, GalleryItem>();
+
+  items.forEach((item) => {
+    if (!item.sight?.label) {
+      return;
+    }
+
+    const existingItem = sightLabelMap.get(item.sight.label);
+
+    if (!existingItem) {
+      sightLabelMap.set(item.sight.label, item);
+      return;
+    }
+
+    const currentCreatedAt = item.image.createdAt;
+    const existingCreatedAt = existingItem.image.createdAt;
+
+    if (currentCreatedAt && !existingCreatedAt) {
+      sightLabelMap.set(item.sight.label, item);
+      return;
+    }
+
+    if (currentCreatedAt && existingCreatedAt) {
+      if (new Date(currentCreatedAt) > new Date(existingCreatedAt)) {
+        sightLabelMap.set(item.sight.label, item);
+      }
+    }
+  });
+
+  const itemsWithSightLabel = Array.from(sightLabelMap.values());
+  const itemsWithoutSightLabel = items.filter((item) => !item.sight?.label);
+
+  return [...itemsWithoutSightLabel, ...itemsWithSightLabel];
 }
