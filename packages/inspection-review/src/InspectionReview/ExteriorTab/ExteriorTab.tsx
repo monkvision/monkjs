e1559829493040407de2c5eef0d844ada537e3ca
Gabriel Tira <gtira@acvauctions.com>
2026-02-10 16:48:18 +0100

    Implemented the structure for passing custom tabs

---
+++ packages/inspection-review/src/InspectionReview/ExteriorTab/ExteriorTab.tsx
@@ -0,0 +1,6 @@
+/**
+ * The ExteriorTab component that displays content based on the currently active tab.
+ */
+export function ExteriorTab() {
+  return <div>Active Tab Content is Exterior</div>;
+}
