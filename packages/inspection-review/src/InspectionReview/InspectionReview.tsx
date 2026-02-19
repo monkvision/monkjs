719587d75d1f22642a56700a9e6597aa60d575f0
Gabriel Tira <gtira@acvautions.com>
2026-02-19 17:17:46 +0100

    Added base components

--- packages/inspection-review/src/InspectionReview/InspectionReview.tsx
+++ packages/inspection-review/src/InspectionReview/InspectionReview.tsx
@@ -1,29 +1,86 @@
-import { i18nWrap, useI18nSync } from '@monkvision/common';
+import { MonkProvider, useI18nSync } from '@monkvision/common';
 import { useTranslation } from 'react-i18next';
 import { styles } from './InspectionReview.styles';
-import { i18nInspectionReview } from '../i18n';
+import { MonkApiConfig } from '@monkvision/network';
+import { InspectionInfo } from './InspectionInfo';
+import { Shortcuts } from './Shortcuts';
+import { Tabs } from './Tabs';
+import { DocumentActions } from './DocumentActions';
+import { ReviewGallery } from './ReviewGallery';
 
 /**
  * Props accepted by the InspectionReview component.
  */
 export type InspectionReviewProps = {
   /**
+   * The ID of the inspection to review.
+   */
+  inspectionId: string;
+  /**
+   * The api config used to communicate with the API. Make sure that the user described in the auth token is the same
+   * one as the one that created the inspection provided in the `inspectionId` prop.
+   */
+  apiConfig: MonkApiConfig;
+  /**
    * This prop can be used to specify the language to be used by the InspectionReview component.
    *
    * @default: en
    */
   lang?: string | null;
+  /**
+   * Additional information to display in the top left corner, apart from VIN number.
+   * Maximum of 2 entries.
+   *
+   * @example
+   *  {
+   *    "License Plate": "ABC-1234",
+   *    "Owner": "John Doe"
+   *  }
+   */
+  additionalInfo?: Record<string, string | number>;
+  /**
+   * Enable or disable PDF generation feature in the document actions.
+   *
+   * @default true
+   */
+  isPDFGeneratorEnabled?: boolean;
+  /**
+   * TODO.
+   */
+  tabs?: string[];
+  /**
+   * Currency to be used for cost estimates.
+   *
+   * @default USD
+   */
+  currency?: string;
 };
 
 /**
  * The Inspection Review component provided by the Monk inspection-review package.
  */
-export const InspectionReview = i18nWrap(function InspectionReview({
-  lang,
-}: InspectionReviewProps) {
+export function InspectionReview({ lang }: InspectionReviewProps) {
   useI18nSync(lang);
   const { t } = useTranslation();
 
-  return <div style={styles['container']}>{t('test')}</div>;
-},
-i18nInspectionReview);
+  return (
+    <MonkProvider>
+      <div style={styles['container']}>
+        <p>{t('test')}</p>
+
+        <div style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+          <InspectionInfo />
+          <Shortcuts />
+        </div>
+        <div style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+          <Tabs />
+          <DocumentActions />
+        </div>
+        <div style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+          <div>Current tabs logic</div>
+          <ReviewGallery />
+        </div>
+      </div>
+    </MonkProvider>
+  );
+}
