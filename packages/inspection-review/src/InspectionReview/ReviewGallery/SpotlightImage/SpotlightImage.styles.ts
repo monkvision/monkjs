6f30d96cdfc9bbe994e86e70c2e4268d9715e4be
Gabriel Tira <gtira@acvauctions.com>
2026-02-19 17:17:47 +0100

    Changed Shortcuts and Spotlight logic and styling

--- packages/inspection-review/src/InspectionReview/ReviewGallery/SpotlightImage/SpotlightImage.styles.ts
+++ packages/inspection-review/src/InspectionReview/ReviewGallery/SpotlightImage/SpotlightImage.styles.ts
@@ -28,6 +28,22 @@ export const styles: Styles = {
     justifyContent: 'space-around',
     width: '100%',
   },
+  iconButton: {
+    width: '40px',
+    height: '40px',
+    backgroundColor: 'transparent',
+    border: 'solid 2px',
+  },
+  imageLabel: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: '8px 12px',
+    fontSize: 16,
+    borderRadius: 9999,
+  },
+  imageLabelIcon: {
+    marginRight: 8,
+  },
   imageContainer: {
     width: '100%',
     height: '100%',
