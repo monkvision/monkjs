fe0acae862d79a3b7ae5b1664ce6e1254a8011c7
Gabriel Tira <gtira@acvautions.com>
2026-03-03 17:00:32 +0100

    Added more logic to Tabs; inspection is fetched in Provider; added todos to decide gallery items

--- packages/inspection-review/src/InspectionReview/InteriorTab/InteriorTab.tsx
+++ packages/inspection-review/src/InspectionReview/InteriorTab/InteriorTab.tsx
@@ -1,6 +1,32 @@
+import { useTabViews } from '../hooks';
+
+enum InteriorViews {
+  DamagesList = 'Damages List',
+  AddDamage = 'Add Damage',
+}
+
 /**
  * The InteriorTab component that displays content based on the currently active tab.
  */
 export function InteriorTab() {
-  return <div>Active Tab Content is Interior</div>;
+  const { currentView, setCurrentView } = useTabViews({ views: Object.values(InteriorViews) });
+
+  if (currentView === InteriorViews.DamagesList)
+    return (
+      <div>
+        <p>Active Tab Content is Interior</p>
+        <div>Displaying Damages List</div>
+        <button onClick={() => setCurrentView(InteriorViews.AddDamage)}>Add Damage</button>
+      </div>
+    );
+
+  return (
+    <div>
+      <p>Active Tab Content is Interior</p>
+      <div>Adding a New Damage</div>
+      <button onClick={() => setCurrentView(InteriorViews.DamagesList)}>
+        Back to Damages List
+      </button>
+    </div>
+  );
 }
