dcdd2c684cb6dc5eb464796b200fd590b6791d23
Gabriel Tira <gtira@acvauctions.com>
2026-06-05 13:35:54 +0200

    Added the base of Inspection Review package

---
+++ packages/inspection-review/src/InspectionReview/InspectionReview.styles.ts
@@ -0,0 +1,14 @@
+import { Styles } from '@monkvision/types';
+
+export const styles: Styles = {
+  container: {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'relative',
+    backgroundColor: 'black',
+  },
+};
