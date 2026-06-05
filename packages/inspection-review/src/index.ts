b6ebf1f5a33ceb39cfeff15f70ab45dc40225334
Gabriel Tira <gtira@acvauctions.com>
2026-06-05 13:38:14 +0200

    Added more languages; implemented PR comments

--- packages/inspection-review/src/index.ts
+++ packages/inspection-review/src/index.ts
@@ -4,5 +4,4 @@ export * from './utils';
 export * from './hooks';
 export * from './config';
 
-export { InspectionReview } from './InspectionReview';
-export { InspectionReviewHOC } from './InspectionReviewHOC';
+export { InspectionReviewHOC as InspectionReview } from './InspectionReviewHOC';
