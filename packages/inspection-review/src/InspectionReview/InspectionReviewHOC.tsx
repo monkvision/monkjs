131489c10c4f5e94643fdf9b6053be9abe9d75b7
Gabriel Tira <gtira@acvautions.com>
2026-06-05 13:35:55 +0200

    Added more logic to Tabs; inspection is fetched in Provider; added todos to decide gallery items

--- packages/inspection-review/src/InspectionReview/InspectionReviewHOC.tsx
+++ packages/inspection-review/src/InspectionReview/InspectionReviewHOC.tsx
@@ -1,7 +1,13 @@
-import { i18nWrap, MonkProvider, useI18nSync } from '@monkvision/common';
+import { i18nWrap, MonkProvider, useI18nSync, useLoadingState } from '@monkvision/common';
 import { i18nInspectionReview } from '../i18n';
 import { InspectionReview, InspectionReviewProps } from './InspectionReview';
 import { InspectionReviewState } from './hooks/InspectionReviewProvider';
+
+// TODO: Props to receive:
+// a single steering wheel direction
+// a list of vehicle type
+//  each vehicle has a list of sights ordered
+//  any sight in the inspection that doesn't match the above list of sights, should go into a specific Tab, decided by the user (mandatory prop)
 
 /**
  * The InspectionReview component is a ready-to-use, single page component that implements an Inspection Review app,
@@ -27,11 +33,16 @@ import { InspectionReviewState } from './hooks/InspectionReviewProvider';
 export const InspectionReviewHOC = i18nWrap(function InspectionReviewHOC(
   props: InspectionReviewProps,
 ) {
+  const loading = useLoadingState(true);
   useI18nSync(props.lang);
 
   return (
     <MonkProvider>
-      <InspectionReviewState inspectionId={props.inspectionId}>
+      <InspectionReviewState
+        inspectionId={props.inspectionId}
+        apiConfig={props.apiConfig}
+        loading={loading}
+      >
         <InspectionReview {...props} />
       </InspectionReviewState>
     </MonkProvider>
